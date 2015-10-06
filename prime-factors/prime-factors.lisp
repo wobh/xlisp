96b93dba97bc216d973e26502451fad78b3edc70
William Clifford <wobh@yahoo.com>
2015-10-06 12:12:07 -0700

    xlisp #23: add stub file to prime-factors exercise.

---
+++ prime-factors/prime-factors.lisp
@@ -0,0 +1,10 @@
+(defpackage #:prime-factors
+  (:use #:cl)
+  (:export #:factors-of)
+  (:documentation "Generates a list of prime factors of given integer."))
+
+(in-package #:prime-factors)
+
+(defun factors-of (number)
+  "List prime factors of `number'."
+  (error "Not implemented."))
